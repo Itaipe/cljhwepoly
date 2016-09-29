dd0325db368d2db4f7529d7d9328735140373d1d
Charles-Henri Jeanjean <chjeab@l4714-12.info.polymtl.ca>
2016-09-29 14:57:42 -0400

    a

--- WebQuiz/routes/index.js
+++ WebQuiz/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'WebQuiz : Accueil', nav: 'false'});
 });
 
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', { title: 'WebQuiz : Tableau de bord', nav: 'true'});
+});
+
 module.exports = router;

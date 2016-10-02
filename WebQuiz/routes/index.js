474c2af21b4cfd21b271a729b823f40c8f25cd6d
Lorent <Loli@Air-de-Lorent.local>
2016-10-01 21:55:07 -0400

    Correction de quelques erreurs

--- WebQuiz/routes/index.js
+++ WebQuiz/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'WebQuiz : Accueil', nav: 'true'});
+  res.render('index', { title: 'WebQuiz : Accueil', nav: 'false'});
 });
 
 router.get('/dashboard', function(req, res, next) {

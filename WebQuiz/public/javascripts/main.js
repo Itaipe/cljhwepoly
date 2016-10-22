a5b424290ee795091817918ea011099aeab8581e
Itaipe <charleshenri.jeanjean@gmail.com>
2016-10-22 19:04:04 -0400

    json debut test

--- WebQuiz/public/javascripts/main.js
+++ WebQuiz/public/javascripts/main.js
@@ -1,5 +1,14 @@
 $(function(){
-  $('.answer .button').on('click', function(e){
-    $.get( '/ajax/next');
+
+    $('.answer .button').on('click', function(e){
+        alert("avanttt get");
+        //$.get('/ajax/next');
+        $.getJSON("/ajax/next", function(data) {
+            alert(data.enonce);
+            $("#domaine").text(data.domaine);
+            $("#enonce").text(data.enonce);
+        });
+        alert("apres get");
   });
+  
 });
--- WebQuiz/routes/ajax.js
+++ WebQuiz/routes/ajax.js
@@ -3,13 +3,22 @@ var router = express.Router();
 
 router.get('/next', function(req, res, next) {
 
-  var val = "Hello World !";
+  var val = "Avec JSON !";
 
   // Ceci pour le debugg
-  console.log(val);
+  console.log(val); 
 
+
+  var question = {
+      "id" : 1,
+      "domaine" : "ExDomaine",
+      "enonce" : "Exemple de question",
+      "reponses" : ["réponse 1", "réponse 2", "réponse 3"]
+  };
+  res.json(eval(question));
+  console.log(question);
   // On revient sur la page de l'examen
-  res.render('examination');
+  //res.render('examination');
 });
 
 module.exports = router;

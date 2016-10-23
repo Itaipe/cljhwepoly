5f721bb58a0d44fb76623d2ca05d6ac8394308c0
Lorent <Loli@MacBook-Air-de-Lorent.local>
2016-10-23 10:29:32 -0400

    Pseudo BD

---
+++ WebQuiz/public/data/data.js
@@ -0,0 +1,21 @@
+questions =
+[
+  {
+    "id" : 1,
+    "domaine"   : "ExDomaine1",
+    "enonce"    : "Exemple de question1",
+    "reponses"  : ["réponse A1", "réponse B1", "réponse C1"]
+  },
+  {
+    "id" : 2,
+    "domaine"   : "ExDomaine2",
+    "enonce"    : "Exemple de question2",
+    "reponses"  : ["réponse A2", "réponse B2", "réponse C2", "réponse D2"]
+  },
+  {
+    "id" : 3,
+    "domaine"   : "ExDomaine3",
+    "enonce"    : "Exemple de question3",
+    "reponses"  : ["réponse A3", "réponse B3", "réponse C3", "réponse D3"]
+  }
+];
--- WebQuiz/routes/ajax.js
+++ WebQuiz/routes/ajax.js
@@ -1,24 +1,10 @@
 var express = require('express');
 var router = express.Router();
+require('./../public/data/data');
 
 router.get('/next', function(req, res, next) {
-
-  var val = "Avec JSON !";
-  // Ceci pour le debugg
-  console.log(val);
-
-
-  var question = {
-      "id" : 1,
-      "domaine" : "ExDomaine",
-      "enonce" : "Exemple de question",
-      "reponses" : ["réponse 1", "réponse 2", "réponse 3"]
-  };
-
-  res.json(eval(question));
-  //console.log(question);
-  // On revient sur la page de l'examen
-  //res.render('examination');
+  res.json(eval(questions[0]));
+  console.log(questions[0]);
 });
 
 module.exports = router;

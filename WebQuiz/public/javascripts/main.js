4f7fb465dc1a17c956cee0db8ccd331f3d488bc7
Lorent <Loli@Air-de-Lorent.local>
2016-10-23 12:28:07 -0400

    Merge

--- WebQuiz/public/javascripts/main.js
+++ WebQuiz/public/javascripts/main.js
@@ -1,6 +1,7 @@
 $(function(){
   $('#boutondetest').on('click', function(e){
     $.getJSON("/ajax/next", function(data) {
+      $("#numero").text("2/2");
       $("#domaine").text(data.domaine);
       $("#enonce").text(data.enonce);
     });

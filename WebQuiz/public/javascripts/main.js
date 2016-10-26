7b6bf3a4ae774fbfa83203bdd10f4218eda01a01
Itaipe <charleshenri.jeanjean@gmail.com>
2016-10-26 16:28:03 -0400

    statistiques ok + bouton remise a zero ok

--- WebQuiz/public/javascripts/main.js
+++ WebQuiz/public/javascripts/main.js
@@ -8,13 +8,25 @@ $(function(){
     var note = 0; // Variable contenant la note
 
     var nbquestionfasttest = 1;
-    
-    
+        
     //Affichage des bonnes statistiques au chargement de la page
-    $(".nb_fasttest_effectues").text(localStorage.getItem("nb_fasttest_effectues"));
-    $(".nb_fasttest_reussis").text(localStorage.getItem("nb_fasttest_reussis"));
-    $(".nb_exam_effectues").text(localStorage.getItem("nb_exam_effectues"));
-    $(".nb_exam_abandonnes").text(localStorage.getItem("nb_exam_abandonnes"));
+    if (localStorage.getItem("nb_fasttest_effectues") !== null) {
+        $(".nb_fasttest_effectues").text(localStorage.getItem("nb_fasttest_effectues"));
+    }
+    if (localStorage.getItem("nb_fasttest_reussis") !== null) {
+        $(".nb_fasttest_reussis").text(localStorage.getItem("nb_fasttest_reussis"));
+    }
+    if (localStorage.getItem("nb_exam_effectues") !== null) {
+        $(".nb_exam_effectues").text(localStorage.getItem("nb_exam_effectues"));
+    }
+    if (localStorage.getItem("nb_exam_abandonnes") !== null) {
+        $(".nb_exam_abandonnes").text(localStorage.getItem("nb_exam_abandonnes"));
+    }
+    if (localStorage.getItem("tauxexam") === null) {
+        $(".tauxexamphrase").text("Aucun examen n'a été effectué pour l'instant");
+    } else {
+        $(".tauxexam").text(localStorage.getItem("tauxexam") + " % ");
+    }
 
     //Si on vient de charger la page test rapide, on génère une question aléatoire
     if (titre === 'WebQuiz : Test Rapide') {
@@ -53,22 +65,46 @@ $(function(){
             }
         });
     }
-
-    //Quand on lance l'exmamen : récupération des paramètres
-    $('#commencerexamen').on('click', function(e){
-        var field = $('#fieldinput').val();
-        nbquestionexam = $('#nbquestioninput').val();
-        //alert(field);
-        //alert(nbquestion);
-        if(typeof(Storage) !== "undefined"){
-            //alert("nbr de clefs stockees avant : " + sessionStorage.length);
-            sessionStorage.setItem("field", field);
-            sessionStorage.setItem("nbquestion", nbquestionexam);
-            //alert("nbr de clefs stockees apres : " + sessionStorage.length);
+    
+    if (titre === 'WebQuiz : Resulat examen') {
+        var result = sessionStorage.getItem("note");
+        var nombreTotaleDeQuestion = sessionStorage.getItem("nbquestion");
+        // ICI IL RESTE PLUS QU'A SAUVEGARDER LA note DANS LA SESSION AFIN QU'ELLE APPARAISSE DANS LES STATS
+        $("#noteFinale").text(result + "/" + nombreTotaleDeQuestion);
+        
+        //Si l'examen a été abandonné (n'a pas été effectué), on ne fait rien
+        if(sessionStorage.getItem("examcourantabandonne")==="true") {
+            sessionStorage.setItem("examcourantabandonne", "false");
         } else {
-            alert("Votre navigateur n'est pas compatible avec le stockage local");
+            //Si c'est le premier examen effectué, on met la variable initiale à 0
+            if (localStorage.getItem("nb_exam_effectues") === null) {
+                localStorage.setItem("nb_exam_effectues", 0);
+            }
+            //On incrémente de 1 le nombre d'examens effetués
+            var nb_exam_effectues = parseInt(localStorage.getItem("nb_exam_effectues")) + 1;
+            localStorage.setItem("nb_exam_effectues", nb_exam_effectues);
+            $(".nb_exam_effectues").text(nb_exam_effectues);
+            
+            //Calcul du nouveau taux de réussite des examens effectués
+            if(localStorage.getItem("nb_question_exam_correctes")===null){
+                var nb_question_exam_correctes = parseInt(result);
+            } else {
+                var nb_question_exam_correctes = parseInt(result) + parseInt(localStorage.getItem("nb_question_exam_correctes"));
+            }
+            if (localStorage.getItem("nb_question_exam_totales")===null) {
+                var nb_question_exam_totales = nombreTotaleDeQuestion;
+            } else {
+                var nb_question_exam_totales = parseInt(nombreTotaleDeQuestion) + parseInt(localStorage.getItem("nb_question_exam_totales"));
+            }
+            localStorage.setItem("nb_question_exam_correctes", nb_question_exam_correctes);
+            localStorage.setItem("nb_question_exam_totales", nb_question_exam_totales);
+            alert("nb_question_exam_correctes : " + nb_question_exam_correctes);
+            alert("nb_question_exam_totales" + nb_question_exam_totales);
+            var tauxexam = parseInt(100 * (nb_question_exam_correctes / nb_question_exam_totales));
+            localStorage.setItem("tauxexam", tauxexam);
+            $(".tauxexam").text(tauxexam + " % ");
         }
-    });
+    }
 
     //Quand on est déja en test rapide, pour obtenir une nouvelle question lorsqu'on valide
     $('#validerfasttest').on('click', function(e){
@@ -84,11 +120,17 @@ $(function(){
             console.log("Bonne réponse !");
             note++;
             //Si la réponse est juste, on change le nombre de fasttest réussis dans les statistiques (et donc dans la BD locale).
+            if (localStorage.getItem("nb_fasttest_reussis") === null) {
+                localStorage.setItem("nb_fasttest_reussis", 0);
+            }
             var nb_fasttest_reussis = parseInt(localStorage.getItem("nb_fasttest_reussis")) + 1;
             localStorage.setItem("nb_fasttest_reussis", nb_fasttest_reussis);
             $(".nb_fasttest_reussis").text(nb_fasttest_reussis);
         }
         //On incrémente de 1 le nombre de fasttest effectues dans les statistiques (et donc dans la BD locale).
+        if (localStorage.getItem("nb_fasttest_effectues") === null) {
+            localStorage.setItem("nb_fasttest_effectues", 0);
+        }
         var nb_fasttest_effectues = parseInt(localStorage.getItem("nb_fasttest_effectues")) + 1;
         localStorage.setItem("nb_fasttest_effectues", nb_fasttest_effectues);
         $(".nb_fasttest_effectues").text(nb_fasttest_effectues);
@@ -108,6 +150,22 @@ $(function(){
                 $('#span'+i).text("  " + data.reponses[i]);
             }
         });
+    });
+    
+    //Quand on lance l'exmamen : récupération des paramètres
+    $('#commencerexamen').on('click', function(e){
+        var field = $('#fieldinput').val();
+        nbquestionexam = $('#nbquestioninput').val();
+        //alert(field);
+        //alert(nbquestion);
+        if(typeof(Storage) !== "undefined"){
+            //alert("nbr de clefs stockees avant : " + sessionStorage.length);
+            sessionStorage.setItem("field", field);
+            sessionStorage.setItem("nbquestion", nbquestionexam);
+            //alert("nbr de clefs stockees apres : " + sessionStorage.length);
+        } else {
+            alert("Votre navigateur n'est pas compatible avec le stockage local");
+        }
     });
 
     //Quand on est déja en examen, pour obtenir une nouvelle question lorsqu'on valide
@@ -132,6 +190,7 @@ $(function(){
         // On fait la redirection vers le resutat du coté du client car passer par le serveur n'est pas nécessaire /!\
         if (nbquestionsexamrestantes === 0) {
           sessionStorage.setItem("note", note);
+          sessionStorage.setItem("examcourantabandonne", "false");
           document.location.href="/examinationResult";
         }
         else {
@@ -151,26 +210,31 @@ $(function(){
           });
         }
     });
-
-    if (titre === 'WebQuiz : Resulat examen') {
-        var result = sessionStorage.getItem("note");
-        var nombreTotaleDeQuestion = sessionStorage.getItem("nbquestion");
-        // ICI IL RESTE PLUS QU'A SAUVEGARDER LA note DANS LA SESSION AFIN QU'ELLE APPARAISSE DANS LES STATS
-        $("#noteFinale").text(result + "/" + nombreTotaleDeQuestion);
-        
-        //On incrémente de 1 le nombre d'examens effetué (ie fini sans avoir abandonné)
-        var nb_exam_effectues = parseInt(localStorage.getItem("nb_exam_effectues")) + 1;
-        localStorage.setItem("nb_exam_effectues", nb_exam_effectues);
-        $(".nb_exam_effectues").text(nb_exam_effectues);
-    }
     
     //Si on abandonne un examen, on va à la page terminale avec la note 0
     $('#abandonexam').on('click', function(e){
         sessionStorage.setItem("note", 0);
+        //Si c'est le premier examen abandonné, on set la variable à 0
+        if (localStorage.getItem("nb_exam_abandonnes") === null) {
+            localStorage.setItem("nb_exam_abandonnes", 0);
+        }        
         var nb_exam_abandonnes = parseInt(localStorage.getItem("nb_exam_abandonnes")) + 1;
         localStorage.setItem("nb_exam_abandonnes", nb_exam_abandonnes);
+        sessionStorage.setItem("examcourantabandonne", "true");
         document.location.href="/examinationResult";
         //On incrémente le nombre d'examen abandonnés
+    });
+    
+    //Quand on clique sur "remise à zéro : on efface tout le contenu du localstorage
+     $('#remiseazero').on('click', function(e){
+        localStorage.clear();
+        sessionStorage.clear();
+        //On change dynamiquement le contenu (pour que l'utilisateur ait un retour sans avoir à recharger la page)
+        $(".nb_fasttest_effectues").text("0");
+        $(".nb_fasttest_reussis").text("0");
+        $(".nb_exam_effectues").text("0");
+        $(".nb_exam_abandonnes").text("0");
+        $(".tauxexamphrase").text("Aucun examen n'a été effectué pour l'instant");
     });
           
 });
